54f5615ac3befe11b1fd49fc4c4031b3e387fa5a
myyrakle <sssang97@naver.com>
2020-12-14 07:54:41 +0900

    add diesel template

--- diesel/src/main.rs
+++ diesel/src/main.rs
@@ -1,7 +1,14 @@
+#[macro_use]
+extern crate diesel;
+
 mod middleware;
 mod route;
+mod lib;
 
 use actix_web::{App, HttpServer}; 
+use actix_web::{web::Data};
+
+use std::sync::{Mutex};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -11,8 +18,11 @@ async fn main() -> std::io::Result<()> {
 
     let _ = listenfd::ListenFd::from_env();
 
+    let db = Data::new(Mutex::new(lib::establish_connection()));
+
     HttpServer::new(move || {
         App::new()
+            .app_data(db.clone())
             .wrap(middleware::Logger::new())
             .service(route::index::helloworld)
             .service(route::index::foobar)
---
+++ diesel/src/schema.rs
@@ -0,0 +1,6 @@
+table! {
+    test (id) {
+        id -> BigInt, 
+        text -> Text,
+    }
+}
